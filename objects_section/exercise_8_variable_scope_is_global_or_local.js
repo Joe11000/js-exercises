f89025f3bc96c75b16f003185514c526040044f3
Joe Noonan <joenoonan27@gmail.com>
2018-12-20 14:09:43 -0600

    block and global scope for variables

---
+++ objects_section/exercise_8_variable_scope_is_global_or_local.js
@@ -0,0 +1,35 @@
+let global_variable = 0
+
+function add1(){
+  global_variable++
+}
+function add2(){
+    global_variable += 2
+}
+function add3(){
+    return {
+        global_variable: global_variable += 3
+    }
+}
+add1(); add2(); add3();
+debugger
+console.log('test_1', global_variable === 6)
+
+
+function dontadd50(){
+    let global_variable = 50
+    global_variable += 50
+    return (global_variable)
+}
+function dontadd150(){
+    const global_variable = {global_variable: undefined}
+    global_variable.global_variable = 250
+    return (global_variable)
+}
+function dontadd250(){
+    var global_variable = 50
+    global_variable += 250
+    return (global_variable)
+}
+dontadd50(); dontadd150(); dontadd250();
+console.log('test_2', global_variable === 6)
